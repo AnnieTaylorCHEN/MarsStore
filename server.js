7c9bdd3724424f6dd83788e235ec797ce364e3e4
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-06-20 15:57:51 +0200

    rewrite nodemailer part with Google OAuth2.0. Previously must authenticate with browser each time.

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express()
 const fs = require('fs')
 const stripe = require('stripe')(stripeSecretKey)
 const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const OAuth2 = google.auth.OAuth2
 const Product = require('./models/products')
 
 const port = process.env.PORT || 3001
@@ -128,51 +130,84 @@ app.get('/contact', (req, res) => {
 })
 
 app.post('/send', (req, res) => {
-    const output = `
-    <h2>Hello, we have received your contact request, and we will get back to you quickly!</h2>
-    <h2>Yours, <h2>
-    <h2>Mars Store</h2>
-    <hr />
-    <h3>Contact Details From You</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Email: ${req.body.email}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `
-   // create reusable transporter object with the default SMTP transport
-   let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'hiamynmax@gmail.com', // generated ethereal user
-            pass: process.env.NODEMAILERPW  // generated ethereal password
-        },
-        tls:{
-        rejectUnauthorized:false
+
+    const sendEmail = async () => {
+
+        const oauth2Client = new OAuth2(
+            process.env.GOOGLE_CLIENT_ID,// ClientID
+            process.env.GOOGLE_CLIENT_SECRET, // Client Secret
+            "https://developers.google.com/oauthplayground" // Redirect URL
+    )
+    //     const GMAIL_SCOPES = [
+    //     'https://mail.google.com/',
+    //     'https://www.googleapis.com/auth/gmail.modify',
+    //     'https://www.googleapis.com/auth/gmail.compose',
+    //     'https://www.googleapis.com/auth/gmail.send',
+    //   ]
+
+    //   const url = oauth2Client.generateAuthUrl({
+    //     access_type: 'offline',
+    //     scope: GMAIL_SCOPES,
+    //   })
+
+    //   const code = '4/bgFIpdWIOJDe_FspTnABMjMxdJoL6Q6_I60XA1gErI7J3IaVnsBEI3T_gtCQBoYD25X0zDsy2E51Vxxa05cd0C8'
+
+    //   const getToken = async () => {
+    //     const { tokens } = await oauth2Client.getToken(code)
+    //     console.info(tokens)
+    //   }
+     
+    //   getToken()
+
+        const output = `
+        <h2>Hello, we have received your contact request, and we will get back to you quickly!</h2>
+        <h2>Yours, <h2>
+        <h2>Mars Store</h2>
+        <hr />
+        <h3>Contact Details From You</h3>
+        <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+    `
+    // create reusable transporter object with the default SMTP transport
+    let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: process.env.GMAIL_ADDRESS,
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+                accessToken: process.env.GOOGLE_ACCESS_TOKEN,
+                expires: Number.parseInt(process.env.GOOGLE_TOKEN_EXPIRE, 10)
+            }
+        })
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+        from: '"Mars Store Customer Support" <hiamynmax@gmail.com>', // sender address
+        to: req.body.email, // list of receivers
+        subject: 'We receive your request - Mars Store', // Subject line
+        html: output // html body
         }
-     })
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-    from: '"Mars Store Customer Support" <hiamynmax@gmail.com>', // sender address
-    to: req.body.email, // list of receivers
-    subject: 'We receive your request - Mars Store', // Subject line
-    html: output // html body
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log("Message sent: %s", info.messageId)
+        })
     }
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error)
-    }
-    console.log("Message sent: %s", info.messageId)
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    sendEmail()
+
     res.render('contact.ejs', { Msg:'Email has been sent!'} )
-})
-
 })
 
 
